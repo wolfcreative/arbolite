713b7b4aff402d6226e319ea2edecac2b4209adc
Hidden Nonsense <loonysnow@gmail.com>
2018-03-21 23:26:06 +0300

    update

--- js/all.js
+++ js/all.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    
+  var needCallback;
+  $('.tenth-shot .show').click(function(e) {
+    e.preventDefault();
+    needCallback = $('.tenth-shot .uk-grid').find('.uk-hidden:lt(3)');
+    if (needCallback.length>0){
+      needCallback.removeClass('uk-hidden')
+    } else {
+      UIkit.notification({ message: 'Отзывы закончились!', status: 'danger', pos: 'top-right', timeout: 5000 });
+    }
+  });
 });
